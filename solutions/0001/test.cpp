3a8127dbc9276c8c733846e7f894b6ad3f6faec5
Viktor Levchenko <Viktor_Levchenko@epam.com>
2023-12-20 15:45:21 +0200

    Base google test for solution 0001.

---
+++ solutions/0001/test.cpp
@@ -0,0 +1,14 @@
+#include "solution.h"
+
+#include <gtest/gtest.h>
+
+using namespace solution0001;
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+    // Create solution object
+    Solution s;
+    // Execute solution
+    auto res = s.twoSum({3,2,4}, 6);
+    // Expectations
+}
