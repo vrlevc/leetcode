fe2c0a1986a336417bb069c899a1c52980e2b637
Viktor Levchenko <Viktor_Levchenko@epam.com>
2023-12-21 11:47:54 +0200

    Cleant up and real test for sol0001

--- solutions/0001/test.cpp
+++ solutions/0001/test.cpp
@@ -1,14 +1,20 @@
 #include "solution.h"
-
+#include <vector>
 #include <gtest/gtest.h>
 
+using namespace std;
 using namespace solution0001;
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-    // Create solution object
-    Solution s;
+TEST(solution0001Test, Test001) {
+    // Expectations:
+    vector<int> exp = {1, 2};
     // Execute solution
-    auto res = s.twoSum({3,2,4}, 6);
-    // Expectations
-}
+    Solution s;
+    vector<int> res = s.twoSum({3,2,4}, 6);
+    // Validate result
+    ASSERT_EQ(res.size(), exp.size()) << "Vectors x and y are of unequal length";
+    for (int i = 0; i < res.size(); ++i) {
+        EXPECT_EQ(res[i], exp[i]) << "Vectors x and y differ at index " << i;
+    }
+} 
